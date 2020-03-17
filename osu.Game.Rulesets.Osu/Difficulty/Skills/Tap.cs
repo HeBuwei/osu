12eaa5788d93c757fda45d57dbb97272ef0f0417
VINXIS <oykxf2@gmail.com>
2020-03-16 19:35:43 -0600

    no test

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Tap.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Tap.cs
@@ -66,6 +66,7 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
                     currStrain += decayCoeffs * currStrainBase * 
                                   Math.Pow(calculateMashNerfFactor(relativeD, mashLevel), 3) *
+                                  Math.Pow(1 + spacedBuff, 3) * 
                                   Math.Pow(1 + fingerStrains[i], 0.6);
 
                     prevPrevTime = prevTime;
